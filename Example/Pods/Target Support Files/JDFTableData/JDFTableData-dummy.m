f738722ab8aca420f1f41fb4ac431c79c63e990d
Joe Fryer <joe.d.fryer@gmail.com>
2015-10-23 11:09:42 +0100

    Project setup.

---
+++ Example/Pods/Target Support Files/JDFTableData/JDFTableData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JDFTableData : NSObject
+@end
+@implementation PodsDummy_JDFTableData
+@end
---
+++ Example/Pods/Target Support Files/JDFTableData/JDFTableData-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double JDFTableDataVersionNumber;
+FOUNDATION_EXPORT const unsigned char JDFTableDataVersionString[];
+
